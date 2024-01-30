b5b9a77156b15042b051cffffd4eadf758275c38
현은비 <silverbi@silverbi-MacBook.local>
2024-01-30 22:12:29 +0900

    feat: Add routing and home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Home from "./pages/home";
+
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <RecoilRoot>
+        <BrowserRouter>
+          {/* <Header /> */}
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            {/* <Route path="*" element={<NotFound />}></Route> */}
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
+  );
 }
 
 export default App;
