fc10c1c07df229896a8af25f05dd61c993f47efd
현은비 <silverbi@silverbi-MacBook.local>
2024-02-03 03:59:53 +0900

    feat: Refactor MapView component to use useLayoutEffect and useRef instead of useEffect for better performance and to avoid unnecessary re-renders

--- src/components/features/place/MapView/index.tsx
+++ src/components/features/place/MapView/index.tsx
@@ -1,15 +1,30 @@
-import { useEffect } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 export const MapView = () => {
-  useEffect(() => {
-    const mapDiv = document.getElementById("map");
-    new window.naver.maps.Map(mapDiv);
+  const mapElement = useRef(null);
+  const { naver } = window;
+  const mapOptions = {
+    scaleControl: true,
+    mapDataControl: true,
+    mapTypeControl: false,
+    zoomControl: false,
+  };
+
+  useLayoutEffect(() => {
+    const location = new naver.maps.LatLng(37.5656, 126.9769);
+    if (!mapElement.current || !naver) return;
+
+    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
   }, []);
 
   return (
     <>
       <div
-        id="map"
+        ref={mapElement}
         className="w-[calc(100vw)] h-[calc(100vh-72px)] min-h-[calc(100vh-72px)]"
       />
     </>
